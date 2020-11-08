cf856beda15902259077f70c136419755f23e29e
lebensjohns <45158773+lebensjohns@users.noreply.github.com>
2020-11-07 23:00:52 -0800

    dependencies in headers

--- main.py
+++ main.py
@@ -2,35 +2,15 @@ import datetime
 
 from flask import Flask, render_template
 from google.cloud import datastore
+from docx import Document
 
 app = Flask(__name__)
 
 datastore_client = datastore.Client()
 
-def store_time(dt):
-    entity = datastore.Entity(key=datastore_client.key('visit'))
-    entity.update({
-        'timestamp': dt
-    })
-
-    datastore_client.put(entity)
-
-
-def fetch_times(limit):
-    query = datastore_client.query(kind='visit')
-    query.order = ['-timestamp']
-
-    times = query.fetch(limit=limit)
-
-    return times
-
 @app.route('/')
 def root():
-    # Store the current access time in Datastore.
-    store_time(datetime.datetime.now())
 
-    # Fetch the most recent 10 access times from Datastore.
-    times = fetch_times(10)
 
     return render_template(
         'index.html', times=times)
